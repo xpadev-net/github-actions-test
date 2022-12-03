4fd9c044de5438d2c72b3befaae75a290bdad616
xpa <xpadev@gmail.com>
2022-12-03 20:13:18 +0900

    wip

---
+++ format.js
@@ -0,0 +1,12 @@
+const message = {
+  "username": "ツール配信bot",
+  "content": "新しいバージョンのリリースのお知らせです😎",
+  "embeds": [
+    {
+      "title":process.env.NAME,
+      "description":process.env.BODY,
+      "color":51711
+    }
+  ]
+};
+console.log(JSON.stringify(message));
